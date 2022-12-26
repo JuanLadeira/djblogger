41d168c5631fb688e3d962b0fa2857138a5c7361
Juan Felipe <juan.fll@hotmail.com>
2022-12-26 20:26:18 -0300

    correção

--- scripts/collecstatic.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /app/
-/opt/venv/bin/python manage.py collectstatic --noinput
--- scripts/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-APP_PORT=${PORT:-8000}
-cd /app/
-/opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm core.wsgi:application
---bind "0.0.0.0:${APP_PORT}"
