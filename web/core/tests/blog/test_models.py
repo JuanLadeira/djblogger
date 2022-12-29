9c2f243355bfae81b103840a9992021b537be789
Juan Felipe <juan.fll@hotmail.com>
2022-12-29 19:25:52 -0300

    implementação de testes unitarios para a criação das tags e para a homepage

--- web/core/tests/blog/test_models.py
+++ web/core/tests/blog/test_models.py
@@ -7,3 +7,7 @@ class TestPostModel:
     def test_str_return(self, post_factory):
         post = post_factory(title="test-post")
         assert post.__str__() == "test-post"
+
+    def test_add_tag(self, post_factory):
+        post = post_factory(title="test-post", tags=["test-tag"])
+        assert post.tags.count() == 1
--- web/core/tests/factories.py
+++ web/core/tests/factories.py
@@ -1,6 +1,9 @@
 import factory
 from core.blog.models import Post
 from django.contrib.auth.models import User
+from factory.faker import faker
+
+FAKE = faker.Faker()
 
 
 class UserFactory(factory.django.DjangoModelFactory):
@@ -18,9 +21,23 @@ class PostFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Post
 
-    title = "x"
-    subtitle = "x"
-    slug = "x"
+    title = factory.Faker("sentence", nb_words=12)
+    subtitle = factory.Faker("sentence", nb_words=12)
+    slug = factory.Faker("slug")
     author = factory.SubFactory(UserFactory)
-    content = "x "
     status = "published"
+
+    @factory.lazy_attribute
+    def content(self):
+        conteudo = ""
+        for _ in range(0, 5):
+            conteudo += "\n" + FAKE.paragraph(nb_sentences=30) + "\n"
+
+        return conteudo
+
+    @factory.post_generation
+    def tags(self, create, extracted, **kwargs):
+        if not create:
+            return
+        if extracted:
+            self.tags.add(*extracted)
