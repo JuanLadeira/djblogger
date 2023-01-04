592c13567b1e312bd2b34a3ced37283002c64a4c
Juan Felipe <juan.fll@hotmail.com>
2023-01-04 00:02:32 -0300

    adicionando banco de dados postgres do digitalocean

--- web/core/cdn/conf.py
+++ web/core/cdn/conf.py
@@ -1,10 +1,7 @@
 from decouple import config
 
 AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
-
-
 AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
-
 AWS_STORAGE_BUCKET_NAME = "djblog-project"
 AWS_S3_ENDPOINT_URL = "https://sfo3.digitaloceanspaces.com"
 AWS_S3_OBJECTS_PAREMETERS = {"CacheControl": "max-age=86400", "ACL": "public-read"}
