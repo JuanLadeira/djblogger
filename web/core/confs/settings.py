826f349540d29bad80dc4c85025cf22e9a20b0e8
Juan Felipe <juan.fll@hotmail.com>
2023-01-03 23:20:34 -0300

    corrigindo configurações de static_root

--- web/core/confs/settings.py
+++ web/core/confs/settings.py
@@ -133,11 +133,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR_OUT / "static-cdn"
-STATICFILES_DIRS = [BASE_DIR_OUT / "static"]
-
-
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR_OUT, "static-cdn")
+STATICFILES_DIRS = [os.path.join(BASE_DIR_OUT, "static")]
 
 
 # Default primary key field type
